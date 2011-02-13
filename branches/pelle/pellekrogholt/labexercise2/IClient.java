d783f568b54f0eb855a75c1845634c5a796432d4
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-02-13 16:05:03 +0000

    various adjusts / cleanups object input/out stream still have to be implemented..

--- branches/pelle/pellekrogholt/labexercise2/IClient.java
+++ branches/pelle/pellekrogholt/labexercise2/IClient.java
@@ -2,24 +2,33 @@ package pellekrogholt.labexercise2;
 
 import java.io.IOException;
 
-public interface IClient<V, K> {
+public interface IClient<V, C> {
 
 	/**
-	 * Send message to server
+	 * Send value to server
 	 * 
-	 * @param message
+	 * @param value
 	 * @throws IOException 
 	 */
-	void send(V message) throws IOException;	
+	void send(V value) throws IOException;	
 	
 	
 	/**
-	 * Send message to server with a command
+	 * Send value to server with a command
 	 * 
 	 * @param command
-	 * @param message
+	 * @param value
 	 * @throws IOException 
 	 */
-	void send(K command, V message) throws IOException;	
+	void send(V value, C command) throws IOException;	
+	
+	
+	
+	/**
+	 * Receive value
+	 * @throws ClassNotFoundException 
+	 * @throws IOException 
+	 */
+	V receive() throws IOException, ClassNotFoundException;
 	
 }
