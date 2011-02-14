4faec0d5b40d6130782411f16b497965169db618
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-02-14 20:32:13 +0000

    prepare labexercise2

---
+++ trunk/distributedsystems/labexercise2/UdpSender.java
@@ -0,0 +1,17 @@
+package pellekrogholt.labexercise2;
+
+import java.net.*;
+import java.io.*;
+
+public class UdpSender {
+  public static void main(String args[ ]) throws Exception{ 
+		InetAddress receiversAddress = InetAddress.getByName("localhost");
+		int receiversPort = 6789;		                                                 
+	  DatagramSocket datagramSocket = new DatagramSocket();
+		String messageAsString = "Hello";
+		byte [ ] message = messageAsString.getBytes();
+		int messageLength = messageAsString.length();
+		DatagramPacket datagramPacket = new DatagramPacket(message, messageLength, receiversAddress, receiversPort);
+    datagramSocket.send( datagramPacket );
+  }
+}
