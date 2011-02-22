0f3ae98fe531347e0ada5d77d9519386e9a98cdf
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-02-22 13:26:56 +0000

    a test for the peron object get over client/server

--- trunk/distributedsystems/labexercise3/TcpServerClientTest.java
+++ trunk/distributedsystems/labexercise3/TcpServerClientTest.java
@@ -10,20 +10,34 @@ public class TcpServerClientTest implements Runnable
 	
 	private static int server_port = 4000;
 	private static int client_port = 4000;
-	private InetAddress server_address;
+	private static InetAddress server_address;
 	
 	/**
 	 * Setup server
 	 * - has to be in a separate thread since server makes a blocking call 
 	 * 
+	 * Important: don't use @Before here since that will call method before each method.
+	 * 
 	 * @throws Throwable
 	 */
-	@Before 
-	public void setupServer() throws Throwable {
+	@BeforeClass 
+	public static void setupServer() throws Throwable {
 		server_address = InetAddress.getByName("localhost");
 		new Thread(new TcpServerClientTest()).start();
 	}
 
+	public static void createPeronObjectsAndSend(TcpClient client, int arg) throws Throwable {
+		Person persons[] = {
+			new Person("Andreas", "Storegade 2", 1020, "60565656"),
+			new Person("Bettina", "Prinsegade 2", 2030, "60565656"),
+			new Person("Thor", "Allergade 4", 4030, "60565656")
+		};
+		for (Person p : persons) {
+			client.send(p, 3);
+		}
+	}	
+	
+	
 	@Override
 	public void run() {
 		try {
@@ -41,5 +55,33 @@ public class TcpServerClientTest implements Runnable
 		Assert.assertEquals(client.send(message, 1), message.toUpperCase());
 		
 	}
+	
+	
+	@Test 
+	public void testRequestPersonObjectFromServer() throws Throwable { 
+		
+		TcpClient client = new TcpClient (client_port, server_address);
+		createPeronObjectsAndSend(client, 3);
+		Assert.assertNotNull(client.send(0, 2));
+		
+	}
 
+	
+	@Test 
+	public void testClientQuitServer() throws Throwable { 
+		
+		TcpClient client = new TcpClient (client_port, server_address);
+		client.send("quit", -1);
+		
+		// should not be possibel
+		String message = "Hello world";
+		System.out.println(client.send(message, 1));
+
+//		setupServer();
+		
+//		Assert.assertEquals(client.send(message, 1), message.toUpperCase());
+
+	}	
+	
+	
 }
