380cddf21cb6fa967ee9c28ad918f6b16c1b755b
bok.chan@gmail.com <bok.chan@gmail.com@38c20fc3-217f-7894-caab-745b74e729db>
2011-02-25 14:00:28 +0000

    Added synchronization of contacts

--- branches/andreas/bok/labexercise4/SynchronizeCommand.java
+++ branches/andreas/bok/labexercise4/SynchronizeCommand.java
@@ -2,10 +2,12 @@ package bok.labexercise4;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Set;
 
 public class SynchronizeCommand extends ServerCommand implements Serializable {
 	Set<ConnectionPoint> cpoints;
+	List<Contact> contacts;
 	SynchronizeStatus status;  
 	
 	public SynchronizeCommand(ConnectionPoint cp1, ConnectionPoint cp2) {
