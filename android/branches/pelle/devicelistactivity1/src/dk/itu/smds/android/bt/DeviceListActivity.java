6af2f88bdcb33437a34ebad2d6b4d2b50e88abb8
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-03-15 12:46:22 +0000

    start up new package

---
+++ android/branches/pelle/devicelistactivity1/src/dk/itu/smds/android/bt/DeviceListActivity.java
@@ -0,0 +1,13 @@
+package dk.itu.smds.android.bt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DeviceListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
