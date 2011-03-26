3e98398fa88ed191fb9a69fd86141ef19974417c
bok.chan <bok.chan@38c20fc3-217f-7894-caab-745b74e729db>
2011-03-26 19:09:07 +0000

    Shit

--- branches/andreas/bok/labexercise8/TcpTest.java
+++ branches/andreas/bok/labexercise8/TcpTest.java
@@ -2,16 +2,16 @@ package bok.labexercise8;
 
 public class TcpTest implements Runnable {
 	public static int port = 1213;
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
 		System.out.println("Exercise optional 8: Secure");
 		String serverIP = "localhost";
-		
+
 		new Thread(new TcpTest()).start();
-		
+
 		SimpleTcpSSLClient stc = new SimpleTcpSSLClient(serverIP,port, "bdsp08-06");
 		// We try to call to the server
 		stc.send(1, "Hello");
@@ -22,7 +22,7 @@ public class TcpTest implements Runnable {
 		System.out.println("Thread called and server created");
 		try {
 			new SimpleTcpSSLServer(port, "bdsp08-06");
-			
+
 		} catch (Exception e) {
 
 			e.printStackTrace();
