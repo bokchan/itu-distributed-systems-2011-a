6b02f42d448b2bfddfc1a55487dd4909a345bc5a
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-04-07 15:37:21 +0000

    start up the phone book

---
+++ pellekrogholt/labexercise10_phonebook/HelloService.java
@@ -0,0 +1,24 @@
+package pellekrogholt.labexercise10;
+
+import javax.jws.WebMethod; 
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+@WebService(name="helloService", targetNamespace = "http://hello.samples.smds2011.itu.dk/",serviceName="helloService")
+public class HelloService 
+{
+	@WebMethod()
+	public String helloOperation(String name) {
+		System.out.println("helloOperation called");
+		
+		return "Hello " + name + "!";
+	}
+	/**
+	* Publish the service end point.
+	* @param args not used.
+	*/
+	public static void main(String[] args) 
+	{
+		Endpoint.publish("http://localhost:8085/labexercises/hello", new HelloService());
+	}
+}
