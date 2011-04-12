230760d896cb4fa6a7d25983b2a689b120333315
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-04-12 07:58:37 +0000

    now with a get person as well - though in the ajax

--- pellekrogholt/labexercise10_phonebook/HelloService.java
+++ pellekrogholt/labexercise10_phonebook/HelloService.java
@@ -1,31 +1,24 @@
 package pellekrogholt.labexercise10_phonebook;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.jws.WebMethod;  
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
-//import distributedsystems.labexercise2.Person;
 
 @WebService(name="helloService", targetNamespace = "http://hello.samples.smds2011.itu.dk/", serviceName="helloService")
 public class HelloService 
 {
-
-	private ArrayList<Person> persons =  new ArrayList<Person>(); 
-	private int count;
-
-	@WebMethod()
-	public String helloOperation(String name) {
-		System.out.println("helloOperation called");
-
-		return "Hello " + name + "!";
-	}
-
 	
+	// for now just store person in memory - simply in a hash map. 
+	private Map<String, Person> map = new HashMap<String, Person>();
 	
+	private int count, count2;
 	
-	// a simpler approach could have been
-	// public String addPerson(Sting name, String street, int phone ...) {
+	/* note: a simpler approach could have been
+	   public String addPerson(Sting name, String street, int phone ...) { */
 	
 	@WebMethod()
 	public String addPerson(Person person) {
@@ -33,13 +26,30 @@ public class HelloService
 		System.out.println("addPerson called "  + ++count );
 		System.out.println("person: "  + person );
 
-		// for now just store person in memory
-		persons.add(person);
+		
+		map.put(person.getName(), person);
 
 		return person.getName() + " added to the phone book service";
-
 	}
 
+	@WebMethod()
+	public String getPerson(String name) {
+		//		Person p = new Person(name, "Last name", 0, null)		
+		System.out.println("getPerson called "  + ++count2 );
+		System.out.println("name "  + name );
+		System.out.println("map.size() "  + map.size());
+
+
+		// for now just store person in memory
+		Person person = map.get(name);
+
+		System.out.println("person: "  + person );
+		
+		return person.toString() + " picked from the the phone book service";
+	}
+	
+	
+	
 
 	/**
 	 * Publish the service end point.
@@ -47,9 +57,6 @@ public class HelloService
 	 */
 	public static void main(String[] args) 
 	{
-
-
-		Endpoint.publish("http://localhost:8085/labexercises/hello", new HelloService());
 		Endpoint.publish("http://localhost:8085/labexercises/person", new HelloService());
 	}
 }
