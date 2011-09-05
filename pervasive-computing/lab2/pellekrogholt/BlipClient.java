94001cd1d56dce5fdb6a163ae864d6835f3862f3
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-09-05 16:37:14 +0000

    lab 2 roughly

---
+++ pervasive-computing/lab2/pellekrogholt/BlipClient.java
@@ -0,0 +1,38 @@
+package lab2.pellekrogholt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+/**
+ * 
+ * Blip Client 1.1
+ *
+ */
+public class BlipClient {
+
+	URL pitlab;
+	
+	public BlipClient(String point) throws IOException {
+		
+		pitlab = new URL("http://pit.itu.dk:7331/location-of/" + point);
+
+
+	}
+		
+	public BufferedReader device() throws IOException {
+
+		URLConnection uc = pitlab.openConnection();
+		BufferedReader in = new BufferedReader(
+			new InputStreamReader(uc.getInputStream())
+		);		
+		return in;
+	}
+	
+	
+
+}
