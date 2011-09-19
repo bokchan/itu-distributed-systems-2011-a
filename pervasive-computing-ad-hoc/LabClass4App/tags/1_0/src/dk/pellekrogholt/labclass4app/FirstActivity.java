3f8e7d9f626abf5d9edb6ad54a1abbb74e6f3488
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-09-19 14:34:08 +0000

    taske 1 done

---
+++ pervasive-computing-ad-hoc/LabClass4App/tags/1_0/src/dk/pellekrogholt/labclass4app/FirstActivity.java
@@ -0,0 +1,20 @@
+package dk.pellekrogholt.labclass4app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    
+    public void goToSecondActivity(View view) {
+    	startActivity(new Intent(this, SecondActivity.class));
+    }
+}
