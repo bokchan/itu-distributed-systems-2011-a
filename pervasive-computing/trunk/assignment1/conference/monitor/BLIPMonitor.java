2d95631fd587d07b01e66c24c7c432ba8d295912
pellekrogholt <pellekrogholt@38c20fc3-217f-7894-caab-745b74e729db>
2011-09-28 13:47:58 +0000

    start up implementing eventbus approach in stead of rest

--- pervasive-computing/trunk/assignment1/conference/monitor/BLIPMonitor.java
+++ pervasive-computing/trunk/assignment1/conference/monitor/BLIPMonitor.java
@@ -18,8 +18,6 @@ import dk.pervasive.jcaf.util.AbstractMonitor;
 public class BLIPMonitor extends AbstractMonitor {
 
 	private Located located = null;
-	private HashMap<String, String> previous_locations;  
-	private static BlipClient client;
 	
 	
 	public BLIPMonitor(String service_uri, Located located) throws RemoteException {
@@ -37,93 +35,68 @@ public class BLIPMonitor extends AbstractMonitor {
 	@Override
 	public void run() {
 
-		// setup thread that keeps listing to blip system for a specific user / mac address
-		while(true) {
-			Thread t = new Thread();
-			t.start();
-			try {
-				System.out.println("BLIPMonitor: thread in blip monitor startet and reading done");
-
-				
-				
-				String point = "E4CE8F3C480D";
-				client = new BlipClient(point);
-				
-				getBlipLocation(client, point);
-				t.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}  
-
+		
+		System.out.println("BLIPMonitor: thread in blip monitor startet and reading done");
 		
 		
-	}
-
-	/**
-	 * 
-	 */
-	private void getBlipLocation(BlipClient client, String mac_id) {
-		// TODO Auto-generated method stub
-		
-		
-		System.out.println("=======================");
-		
-		System.out.println("client");
-		
-		System.out.println("=======================");
-		
-		
-//		HashMap<String, String> map; 
-//		if ((map = (HashMap<String, String>) JSON.parse(client.toString())) != null) {
-//			
-//			String location = map.get("location");
-//			if (!location.equals(previous_locations.get(mac_id))) {
-//				System.out.println(location);
+//		// setup thread that keeps listing to blip system for a specific user / mac address
+//		while(true) {
+//			Thread t = new Thread();
+//			t.start();
+//			try {
+//				System.out.println("BLIPMonitor: thread in blip monitor startet and reading done");
+//
+//				
+//				
+//				String point = "E4CE8F3C480D";
+//				client = new BlipClient(point);
+//				
+//				getBlipLocation(client, point);
+//				t.sleep(5000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
 //			}
-//			previous_locations.put(mac_id, location);
-//			
-//		} else {
-//			System.out.println("Device has not turned on bluetooth");
-//		}
-		
-		
-//		E4CE8F3C480D
+//		}  
+
 		
 		
 	}
 
-	
-	/**
-	 * 
-	 * Blip Client 1.1
-	 *
-	 */
-	private class BlipClient {
-
-		URL pitlab;
-		
-		public BlipClient(String point) throws IOException {
-			
-			pitlab = new URL("http://pit.itu.dk:7331/location-of/" + point);
-
-		}
-			
-		public BufferedReader device() throws IOException {
-
-			URLConnection uc = pitlab.openConnection();
-			BufferedReader in = new BufferedReader(
-				new InputStreamReader(uc.getInputStream())
-			);		
-			return in;
-		}
-		
-		
-
-	}
+//	/**
+//	 * 
+//	 */
+//	private void getBlipLocation(BlipClient client, String mac_id) {
+//		// TODO Auto-generated method stub
+//		
+//		
+//		System.out.println("=======================");
+//		
+//		System.out.println("client");
+//		
+//		System.out.println("=======================");
+//		
+//		
+////		HashMap<String, String> map; 
+////		if ((map = (HashMap<String, String>) JSON.parse(client.toString())) != null) {
+////			
+////			String location = map.get("location");
+////			if (!location.equals(previous_locations.get(mac_id))) {
+////				System.out.println(location);
+////			}
+////			previous_locations.put(mac_id, location);
+////			
+////		} else {
+////			System.out.println("Device has not turned on bluetooth");
+////		}
+//		
+//		
+////		E4CE8F3C480D
+//		
+//		
+//	}
 	
 }
